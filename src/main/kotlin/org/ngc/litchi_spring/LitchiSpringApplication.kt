e08c8aa300e8f1fbb2d3723492907414b888ca45
ngc2207 <ngc3109@gmail.com>
2024-10-15 18:49:16 +0800

    first commit

---
+++ src/main/kotlin/org/ngc/litchi_spring/LitchiSpringApplication.kt
@@ -0,0 +1,11 @@
+package org.ngc.litchi_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LitchiSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<LitchiSpringApplication>(*args)
+}
---
+++ src/test/kotlin/org/ngc/litchi_spring/LitchiSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ngc.litchi_spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LitchiSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
